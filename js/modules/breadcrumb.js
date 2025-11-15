aa461e205e251b14eb09e5c9f0eabe2cd0875a47
duongdada <anhlavodoivt123@gmail.com>
2025-11-15 13:02:13 +0700

    modify

---
+++ js/modules/breadcrumb.js
@@ -0,0 +1,27 @@
+// breadcrumb.js
+function setBreadcrumb() {
+    const breadcrumbList = document.getElementById('breadcrumb-list');
+    if (!breadcrumbList) return;
+
+    const currentPath = window.location.pathname.split("/").pop();
+
+    const pageNames = {
+        "collection.html": "Bộ sưu tập nước hoa",
+        "detail.html": "Chi tiết sản phẩm",
+        "login.html": "Đăng nhập",
+        "register.html": "Đăng ký",
+        "admin.html": "Trang quản trị"
+    };
+
+    breadcrumbList.innerHTML = `
+        <li class="breadcrumb-item">
+            <a href="../index.html">Trang chủ</a>
+        </li>
+        <li class="breadcrumb-separator">
+            <i class="fa-solid fa-chevron-right"> </i>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+            ${pageNames[currentPath]}
+        </li>
+    `;
+}
--- js/modules/detail.js
+++ js/modules/detail.js
@@ -11,4 +11,7 @@ $(document).ready(function () {
     // Hiển thị sẵn số lượng của nút được chọn mặc định (10ml)
     const defaultStock = $(".volume-btn.active").data("stock");
     $("#stockDisplay").text(defaultStock + " sản phẩm");
+
+
+
 });
